14366ab28a6fbd7f0016c357187463ae3e2e9f56
Andrew Cotter <cotter.andrew.p@gmail.com>
2022-09-18 16:32:45 -0400

    week 1 example

--- week01/p5-start/script.js
+++ week01/p5-start/script.js
@@ -1,7 +1,31 @@
+let x = 180
+let y = 150
+
+let r = 185
+let g = 80
+let b = 129
+
+let myName = "Andrew"
+
 function setup() {
-	createCanvas(800, 600);
+  createCanvas(800, 600)
 }
 
 function draw() {
-	background(0);
+  background(r, g, b)
+  
+  stroke(255, 255, 255)
+  fill(0, 0, 255)
+  ellipse(x, y, 25, 25)
+  
+  stroke(255, 0, 255)
+  fill(255, 0, 0)
+  // rectMode(CENTER)
+  rect(0, 100, 50, 25)
+  
+  stroke(0,0,0);
+  line(0, 0, width, height);
+  
+  fill(255)
+  text( myName ,50, 50)
 }
