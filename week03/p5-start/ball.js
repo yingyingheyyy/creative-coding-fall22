266f0109177c65cc8cf2c53f708a288167befb34
Andrew Cotter <cotter.andrew.p@gmail.com>
2022-09-26 21:34:19 -0400

    objects example

---
+++ week03/p5-start/ball.js
@@ -0,0 +1,19 @@
+class Ball {
+    constructor(x, y, c){
+      this.x = x
+      this.y = y
+      this.speedX = random(-1,1)
+      this.speedY = random(-1,1)
+      this.color = c//color(128, 64, 185)
+    }
+  
+    move(){
+      this.x += this.speedX
+      this.y += this.speedY
+    }
+  
+    display(){
+      fill(this.color)
+      ellipse(this.x, this.y, 100)
+    }
+  }
--- week03/p5-start/script.js
+++ week03/p5-start/script.js
@@ -1,9 +1,33 @@
-
+let ballArray = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
+
+  for(let i = 0; i < 100; i++){
+    ballArray.push(new Ball(random(width), random(height), 
+    color(random(255), random(255), random(255))))
+  }
+
+  console.log(ballArray)
 }
 
 function draw() {
   background(100)
+
+  for (let i = 0; i < ballArray.length; i++) {
+    // ellipse(ballArray[i].x, ballArray[i].y, 100)
+    ballArray[i].move()
+    ballArray[i].display()
+  }
+  // ballArray.forEach((ball) => {
+  //   ellipse(ball.x, ball.y, 100)
+  // })
 }
+
+// function makeBall(ballX, ballY) {
+//   let ball = {
+//     x: ballX,
+//     y: ballY
+//   }
+//   return ball
+// }
