2cd81c2468bf6df8fb0c30702c3e9d174c2598b7
Andrew Cotter <cotter.andrew.p@gmail.com>
2022-10-03 14:33:18 -0400

    format

--- week04/p5-start/script.js
+++ week04/p5-start/script.js
@@ -3,9 +3,9 @@ let ballArray = []
 function setup() {
   createCanvas(windowWidth, windowHeight)
 
-  for(let i = 0; i < 100; i++){
-    ballArray.push(new Ball(random(width), random(height), 
-    color(random(255), random(255), random(255))))
+  for (let i = 0; i < 100; i++) {
+    ballArray.push(new Ball(random(width), random(height),
+      color(random(255), random(255), random(255))))
   }
 
   console.log(ballArray)
